b2842c5f79bf7553d1a782ba19c156afaf227d80
Reem Ahmed <reem.mda0909@gmail.com>
2022-05-17 07:27:22 -0600

    install dotenv and set up mysql2 database

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,10 @@
-require('dotenv').config();
+// import the Sequelize constructor from the library
 
 const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// create connection to our database, pass in your MySQL information for username and password
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
